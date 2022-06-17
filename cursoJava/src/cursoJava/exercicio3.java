6486df06d2aa1e11e0e5ffbbeb76556736300347
Patroc01 <marlupatrocinio01@gmail.com>
2022-06-17 15:47:53 -0300

    atualizando git

--- cursoJava/src/cursoJava/exercicio3.java
+++ cursoJava/src/cursoJava/exercicio3.java
@@ -5,6 +5,10 @@ public class exercicio3 {
 
 	public static void main(String[] args) {
 
+		/*
+		 * Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
+de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+		 */
 		Scanner sc = new Scanner(System.in);
 		
 		int A, B, C, D;
@@ -15,6 +19,8 @@ public class exercicio3 {
 		D = sc.nextInt();
 		
 		System.out.println("DIFERENCA = " + (A * B - C * D));
+		
+		sc.close();
 		
 	}
 

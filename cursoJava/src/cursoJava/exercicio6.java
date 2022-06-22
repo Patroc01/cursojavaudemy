3f52ff43fbfcc176b52bb0f78fbe08d7bedb96aa
Patroc01 <marlupatrocinio01@gmail.com>
2022-06-22 11:37:30 -0300

    Att

--- cursoJava/src/cursoJava/exercicio6.java
+++ cursoJava/src/cursoJava/exercicio6.java
@@ -6,7 +6,7 @@ public class exercicio6 {
 
 	public static void main(String[] args) {
 	/*
-	 * 	Fazer um programa que leia três valores com ponto flutuante de dupla precisão: 
+	 *Fazer um programa que leia três valores com ponto flutuante de dupla precisão: 
 	 * A, B e C. Em seguida, calcule e mostre:
 a) a área do triângulo retângulo que tem A por base e C por altura.
 b) a área do círculo de raio C. (pi = 3.14159)
@@ -36,7 +36,6 @@ e) a área do retângulo que tem lados A e B.
 		System.out.printf("TRAPEZIO: %.3f%n", trapezio);
 		System.out.printf("QUADRADO: %.3f%n", quadrado);
 		System.out.printf("RETANGULO: %.3f%n", retangulo);
-		
 		
 		sc.close();
 	}

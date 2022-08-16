19a2710fded77803aa8188f1f778a04e63e4720c
Marlu Patrocinio <marlupatrocinio01@gmail.com>
2022-08-16 12:38:58 -0300

    aulas

--- cursoJava/src/cursoJava/Main.java
+++ cursoJava/src/cursoJava/Main.java
@@ -4,14 +4,11 @@ package cursoJava;
 public class Main {
 	public static void main(String[] args) {
 
-		double x = 100;
-		double y = 100;
-		
-		while (x != y) {
-			System.out.println("olha");
-			x = Math.sqrt(y);
-
-		}
-		
+	String x = "asdfg ASDFG ASfa aFda ADSADA    ";
+	
+	String y = x.toLowerCase();
+	
+	System.out.println(y);
 	}
+
 }
